b586ce56c00a07fb2dfd12a82fa5c5a223aed4f2
kochetova <kochetova@protei.ru>
2023-02-27 18:28:37 +0300

    Initial commit

---
+++ tests/test_mail_1.py
@@ -0,0 +1,10 @@
+import pytest
+
+def test_sending_mail_1(set_up, some):   # в скобках прописываем то что будет выполняться до шагов
+    """Список шагов"""
+    print('Письмо отправлено')
+
+# test_sending_mail()
+
+def test_sending_mail_2(set_up, some):
+    print('Письмо отправлено')
---
+++ tests/test_mail_2.py
@@ -0,0 +1,10 @@
+import pytest
+
+def test_sending_mail_3(set_up):   # в скобках прописываем то что будет выполняться до шагов
+    """Список шагов"""
+    print('ooooo')
+
+# test_sending_mail()
+
+def test_sending_mail_4(set_up):
+    print('kkkk')
